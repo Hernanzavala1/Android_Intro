5e7980ecd18e18bdcbfd976f05071092882347a7
Hernan Zavala <hernanjosuezavala@hotmail.com>
2018-09-23 15:15:34 -0500

    Linear layout lessson

--- app/src/main/java/com/example/herna/android_studio_intro/Layout_lessons/MainActivity.java
+++ app/src/main/java/com/example/herna/android_studio_intro/Layout_lessons/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.herna.android_studio_intro;
+package com.example.herna.android_studio_intro.Layout_lessons;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import com.example.herna.android_studio_intro.R;
 
 public class MainActivity extends Activity {
 
